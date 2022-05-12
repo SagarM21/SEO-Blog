c7d2638d43767977830bccda3258770466980ecc
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2022-05-12 10:14:15 +0530

    Signup validator

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,10 @@ import express from "express";
 const router = express.Router();
 import { signup } from "../controllers/auth.js";
 
-router.post("/signup", signup);
+// validators
+import { userSignupValidator } from "../validators/auth.js";
+import { runValidation } from "../validators/index.js";
+
+router.post("/signup", userSignupValidator, runValidation, signup);
 
 export default router;
