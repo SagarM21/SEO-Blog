e2ae09065f246f5a64f9dd4c538cdc80c4984143
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2022-05-12 10:36:58 +0530

    User signup

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,5 +1,5 @@
-const mongoose = require("mongoose");
-const crypto = require("crypto");
+import mongoose from "mongoose";
+import crypto from "crypto";
 
 const userSchema = new mongoose.Schema(
 	{
@@ -88,4 +88,4 @@ userSchema.methods = {
 	},
 };
 
-module.exports = mongoose.model("User", userSchema);
+export default mongoose.model("User", userSchema);
