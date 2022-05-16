86f484c9155fa16174d37d27a7caeed5bfb6c458
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2022-05-16 15:19:21 +0530

    Create blog with categories and tags

--- backend/controllers/blog.js
+++ backend/controllers/blog.js
@@ -20,6 +20,32 @@ export const create = (req, res) => {
 
 		const { title, body, categories, tags } = fields;
 
+		// this code is showing title is required always - idk
+
+		// if (!title || title.length) {
+		// 	return res.status(400).json({
+		// 		error: "Title is required",
+		// 	});
+		// }
+
+		// if (!body || body.length < 200) {
+		// 	return res.status(400).json({
+		// 		error: "Content is too short",
+		// 	});
+		// }
+
+		// if (!categories || categories.length === 0) {
+		// 	return res.status(400).json({
+		// 		error: "At least one category is required",
+		// 	});
+		// }
+
+		// if (!tags || tags.length === 0) {
+		// 	return res.status(400).json({
+		// 		error: "At least one tag is required",
+		// 	});
+		// }
+
 		let blog = new Blog();
 		blog.title = title;
 		blog.body = body;
@@ -27,6 +53,10 @@ export const create = (req, res) => {
 		blog.mtitle = `${title} | ${process.env.APP_NAME}`;
 		blog.mdesc = stripHtml(body.substring(0, 160)).result; // gives out only text
 		blog.postedBy = req.user._id;
+
+		// categories and tags
+		let arrayOfCategories = categories && categories.split(",");
+		let arrayOfTags = tags && tags.split(",");
 
 		if (files.photo) {
 			if (files.photo.size > 10000000) {
@@ -44,7 +74,32 @@ export const create = (req, res) => {
 					error: errorHandler(err),
 				});
 			}
-			res.json(result);
+			//res.json(result);
+			Blog.findByIdAndUpdate(
+				result._id,
+				{ $push: { categories: arrayOfCategories } },
+				{ new: true }
+			).exec((err, result) => {
+				if (err) {
+					return res.status(400).json({
+						error: errorHandler(err),
+					});
+				} else {
+					Blog.findByIdAndUpdate(
+						result._id,
+						{ $push: { tags: arrayOfTags } },
+						{ new: true }
+					).exec((err, result) => {
+						if (err) {
+							return res.status(400).json({
+								error: errorHandler(err),
+							});
+						} else {
+							res.json(result);
+						}
+					});
+				}
+			});
 		});
 	});
 };
