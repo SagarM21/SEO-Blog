727fd8dfd777a9f3c1454b830f86821658ce22d2
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2022-05-18 16:16:07 +0530

    SSR blog page with getInitialProps

--- frontend/pages/blogs/index.js
+++ frontend/pages/blogs/index.js
@@ -4,8 +4,53 @@ import Layout from "../../components/Layout";
 import React, { useState } from "react";
 import { listBlogsWithCategoriesAndTags } from "../../actions/blog";
 import { API } from "../../config";
+import renderHTML from "react-render-html";
+import moment from "moment";
 
-const Blogs = () => {
+const Blogs = ({ blogs, categories, tags, size }) => {
+	const showAllBlogs = () => {
+		return blogs.map((blog, i) => {
+			// curly brace use kro to return lagao
+			return (
+				<article key={i}>
+					<div className='lead pb-4'>
+						<header>
+							<Link href={`/blogs/${blog.slug}`}>
+								<a>
+									<h2 className=' pt-3 pb-3 font-weight-bold'>{blog.title}</h2>
+								</a>
+							</Link>
+						</header>
+						<section>
+							<p className='mark ml-1 pt-2 pb-2'>
+								Written by {blog.postedBy.name} | Published{" "}
+								{moment(blog.updatedAt).fromNow()}
+							</p>
+						</section>
+
+						<section>
+							<p>blog categories and tags</p>
+						</section>
+
+						<div className='row'>
+							<div className='col-md-4'>Image</div>
+							<div className='col-md-8'>
+								<section>
+									<div className='pb-3'>
+										{blog.excerpt == undefined ? "" : renderHTML(blog.excerpt)}
+									</div>
+									<Link href={`/blogs/${blog.slug}`}>
+										<a className='btn btn-primary pt-2'>Read More</a>
+									</Link>
+								</section>
+							</div>
+						</div>
+					</div>
+					<hr />
+				</article>
+			);
+		});
+	};
 	return (
 		<Layout>
 			<main>
@@ -23,12 +68,27 @@ const Blogs = () => {
 				</div>
 				<div className='container-fluid'>
 					<div className='row'>
-						<div className='col-md-12'>Show all blogs</div>
+						<div className='col-md-12'>{showAllBlogs()}</div>
 					</div>
 				</div>
 			</main>
 		</Layout>
 	);
+};
+
+Blogs.getInitialProps = () => {
+	return listBlogsWithCategoriesAndTags().then((data) => {
+		if (data.error) {
+			console.log(data.error);
+		} else {
+			return {
+				blogs: data.blogs,
+				categories: data.categories,
+				tags: data.tags,
+				size: data.size,
+			};
+		}
+	});
 };
 
 export default Blogs;
