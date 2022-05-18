2168b0843f3f41f3b653d39aef23fc605b17933e
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2022-05-19 00:06:45 +0530

    Load more blogs - frontend

--- frontend/actions/blog.js
+++ frontend/actions/blog.js
@@ -17,12 +17,18 @@ export const createBlog = (blog, token) => {
 		.catch((err) => console.log(err));
 };
 
-export const listBlogsWithCategoriesAndTags = (blog, token) => {
+export const listBlogsWithCategoriesAndTags = (skip, limit) => {
+	const data = {
+		limit,
+		skip,
+	};
 	return fetch(`${API}/blogs-categories-tags`, {
 		method: "POST",
 		headers: {
 			Accept: "application/json",
+			"Content-Type": "application/json",
 		},
+		body: JSON.stringify(data),
 	})
 		.then((response) => {
 			return response.json();
--- frontend/pages/blogs/index.js
+++ frontend/pages/blogs/index.js
@@ -7,7 +7,15 @@ import { listBlogsWithCategoriesAndTags } from "../../actions/blog";
 import { API, DOMAIN, APP_NAME, FB_APP_ID } from "../../config";
 import Card from "../../components/blog/Card";
 
-const Blogs = ({ blogs, categories, tags, size, router }) => {
+const Blogs = ({
+	blogs,
+	categories,
+	tags,
+	totalBlogs,
+	blogsLimit,
+	blogSkip,
+	router,
+}) => {
 	const head = () => {
 		<Head>
 			<title>Programming blogs | {APP_NAME}</title>
@@ -37,6 +45,34 @@ const Blogs = ({ blogs, categories, tags, size, router }) => {
 		</Head>;
 	};
 
+	const [limit, setLimit] = useState(blogsLimit);
+	const [skip, setSkip] = useState(0);
+	const [size, setSize] = useState(totalBlogs);
+	const [loadedBlogs, setLoadedBlogs] = useState([]);
+
+	const loadMore = () => {
+		let toSkip = skip + limit;
+		listBlogsWithCategoriesAndTags(toSkip, limit).then((data) => {
+			if (data.error) {
+				console.log(data.error);
+			} else {
+				setLoadedBlogs([...loadedBlogs, ...data.blogs]);
+				setSize(data.size);
+				setSkip(toSkip);
+			}
+		});
+	};
+
+	const loadMoreButton = () => {
+		return (
+			size > 0 &&
+			size >= limit && (
+				<button onClick={loadMore} className='btn btn-outline-primary btn-lg'>
+					Load more
+				</button>
+			)
+		);
+	};
 	const showAllBlogs = () => {
 		return blogs.map((blog, i) => {
 			// curly brace use kro to return lagao
@@ -65,6 +101,14 @@ const Blogs = ({ blogs, categories, tags, size, router }) => {
 		));
 	};
 
+	const showLoadedBlogs = () => {
+		return loadedBlogs.map((blog, i) => (
+			<article key={i}>
+				<Card blog={blog} />
+			</article>
+		));
+	};
+
 	return (
 		<React.Fragment>
 			{head()}
@@ -86,11 +130,9 @@ const Blogs = ({ blogs, categories, tags, size, router }) => {
 							</section>
 						</header>
 					</div>
-					<div className='container-fluid'>
-						<div className='row'>
-							<div className='col-md-12'>{showAllBlogs()}</div>
-						</div>
-					</div>
+					<div className='container-fluid'>{showAllBlogs()}</div>
+					<div className='container-fluid'>{showLoadedBlogs()} </div>
+					<div className='text-center pt-5 pb-5'>{loadMoreButton()}</div>
 				</main>
 			</Layout>
 		</React.Fragment>
@@ -98,7 +140,10 @@ const Blogs = ({ blogs, categories, tags, size, router }) => {
 };
 
 Blogs.getInitialProps = () => {
-	return listBlogsWithCategoriesAndTags().then((data) => {
+	let skip = 0;
+	let limit = 2;
+
+	return listBlogsWithCategoriesAndTags(skip, limit).then((data) => {
 		if (data.error) {
 			console.log(data.error);
 		} else {
@@ -106,7 +151,9 @@ Blogs.getInitialProps = () => {
 				blogs: data.blogs,
 				categories: data.categories,
 				tags: data.tags,
-				size: data.size,
+				totalBlogs: data.size,
+				blogsLimit: limit,
+				blogSkip: skip,
 			};
 		}
 	});
