25a0991e6d2cf8971f4b8a094dcff8ddecbab587
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2022-05-20 21:58:27 +0530

    Tags with all blogs and implemented SEO

--- backend/controllers/tag.js
+++ backend/controllers/tag.js
@@ -1,4 +1,5 @@
 import Tag from "../models/tags.js";
+import Blog from "../models/blog.js";
 import slugify from "slugify";
 import { errorHandler } from "../helpers/dbErrorHandler.js";
 
@@ -45,12 +46,27 @@ export const remove = (req, res) => {
 
 export const read = (req, res) => {
 	const slug = req.params.slug.toLowerCase();
-	Tag.findOne({ slug }).exec((err, category) => {
+	Tag.findOne({ slug }).exec((err, tag) => {
 		if (err) {
 			return res.status(400).json({
 				error: errorHandler(err),
 			});
 		}
-		res.json(category);
+		//res.json(tag);
+		Blog.find({ tags: tag })
+			.populate("categories", "_id name slug")
+			.populate("tags", "_id name slug")
+			.populate("postedBy", "_id name")
+			.select(
+				"_id title slug excerpt categories postedBy tags createdAt updatedAt"
+			)
+			.exec((err, data) => {
+				if (err) {
+					return res.status(400).json({
+						error: errorHandler(err),
+					});
+				}
+				res.json({ tag: tag, blogs: data }); // now in frontend use them like data.category or data.blogs
+			});
 	});
 };
--- frontend/pages/tags/[slug].js
+++ frontend/pages/tags/[slug].js
@@ -2,27 +2,27 @@ import Head from "next/head";
 import Link from "next/link";
 import Layout from "../../components/Layout";
 import React from "react";
-import { singleCategory } from "../../actions/category";
+import { singleTag } from "../../actions/tag";
 import { API, DOMAIN, APP_NAME, FB_APP_ID } from "../../config";
 import renderHTML from "react-render-html";
 import moment from "moment";
 import Card from "../../components/blog/Card";
 
-const Category = ({ category, blogs, query }) => {
+const Tag = ({ tag, blogs, query }) => {
 	const head = () => (
 		<Head>
 			<title>
-				{category.name} | {APP_NAME}
+				{tag.name} | {APP_NAME}
 			</title>
 			<meta
 				name='description'
-				content={`Best programming tutorials on ${category.name}`}
+				content={`Best programming tutorials on ${tag.name}`}
 			/>
 			<link rel='canonical' href={`${DOMAIN}/categories/${query.slug}`} />
-			<meta property='og:title' content={`${category.name}| ${APP_NAME}`} />
+			<meta property='og:title' content={`${tag.name}| ${APP_NAME}`} />
 			<meta
 				property='og:description'
-				content={`Best programming tutorials on ${category.name}`}
+				content={`Best programming tutorials on ${tag.name}`}
 			/>
 			<meta property='og:type' content='webiste' />
 			<meta property='og:url' content={`${DOMAIN}/categories/${query.slug}`} />
@@ -40,15 +40,15 @@ const Category = ({ category, blogs, query }) => {
 			<meta property='fb:app_id' content={`${FB_APP_ID}`} />
 		</Head>
 	);
-
 	return (
 		<React.Fragment>
+			{head()}
 			<Layout>
 				<main>
 					<div className='container-fluid text-center'>
 						<header>
 							<div className='col-md-12 pt-3'>
-								<h1 className='display-4 font-weight-bold'>{category.name}</h1>
+								<h1 className='display-4 font-weight-bold'>{tag.name}</h1>
 								{blogs.map((b, i) => (
 									<div>
 										<Card blog={b} key={i} />
@@ -65,14 +65,14 @@ const Category = ({ category, blogs, query }) => {
 };
 
 // to make server side rendering
-Category.getInitialProps = ({ query }) => {
-	return singleCategory(query.slug).then((data) => {
+Tag.getInitialProps = ({ query }) => {
+	return singleTag(query.slug).then((data) => {
 		if (data.error) {
 			console.log(data.error);
 		} else {
-			return { category: data.category, blogs: data.blogs, query };
+			return { tag: data.tag, blogs: data.blogs, query };
 		}
 	});
 };
 
-export default Category;
+export default Tag;
