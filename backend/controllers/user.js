b109dcf646b8e34a3cf298a68cf11482cf54f3a5
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2022-05-21 16:04:39 +0530

    User public profile backend

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,6 +1,43 @@
 import User from "../models/user.js";
+import Blog from "../models/blog.js";
+import { errorHandler } from "../helpers/dbErrorHandler.js";
 
 export const read = (req, res) => {
 	req.profile.hashed_password = undefined;
 	return res.json(req.profile);
+};
+
+export const publicProfile = (req, res) => {
+	let username = req.params.username;
+	let user;
+	let blogs;
+	User.findOne({ username }).exec((err, userFromDB) => {
+		if (err || !userFromDB) {
+			return res.status(400).json({
+				error: "User not found",
+			});
+		}
+		user = userFromDB;
+		let userId = user._id;
+		Blog.find({ postedBy: userId })
+			.populate("categories", "_id name slug")
+			.populate("tags", "_id name slug")
+			.populate("postedBy", "_id name")
+			.limit(10)
+			.select(
+				"_id title slug excerpt categories tags postedBy createdAt updatedAt"
+			)
+			.exec((err, data) => {
+				if (err) {
+					return res.status(400).json({
+						error: errorHandler(err),
+					});
+				}
+				user.photo = undefined;
+				res.json({
+					user,
+					blogs: data,
+				});
+			});
+	});
 };
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,8 +1,9 @@
 import express from "express";
 const router = express.Router();
 import { requireSignin, authMiddleware } from "../controllers/auth.js";
-import { read } from "../controllers/user.js";
+import { read, publicProfile } from "../controllers/user.js";
 
 router.get("/profile", requireSignin, authMiddleware, read);
+router.get("/user/:username", publicProfile);
 
 export default router;
