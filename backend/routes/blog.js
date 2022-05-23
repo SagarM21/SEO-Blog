60442188b3e01f4364cffb55affd4002bca56544
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2022-05-23 08:42:15 +0530

    Blog create for auth user

--- backend/routes/blog.js
+++ backend/routes/blog.js
@@ -11,7 +11,11 @@ import {
 	listRelated,
 	listSearch,
 } from "../controllers/blog.js";
-import { requireSignin, adminMiddleware } from "../controllers/auth.js";
+import {
+	requireSignin,
+	adminMiddleware,
+	authMiddleware,
+} from "../controllers/auth.js";
 
 router.post("/blog", requireSignin, adminMiddleware, create);
 router.get("/blogs", list);
@@ -22,5 +26,10 @@ router.put("/blog/:slug", requireSignin, adminMiddleware, update);
 router.get("/blog/photo/:slug", photo);
 router.post("/blogs/related", listRelated);
 router.get("/blogs/search", listSearch);
+
+// auth user blog crud
+router.post("/user/blog", requireSignin, authMiddleware, create);
+router.delete("/user/blog/:slug", requireSignin, authMiddleware, remove);
+router.put("/user/blog/:slug", requireSignin, authMiddleware, update);
 
 export default router;
--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,25 +1,28 @@
 import React, { useState } from "react";
-import { APP_NAME } from "../config";
 import Link from "next/link";
-import { isAuth, signout } from "../actions/auth";
-import {
-	Navbar,
-	NavbarBrand,
-	NavbarToggler,
-	Nav,
-	Collapse,
-	NavLink,
-	NavItem,
-} from "reactstrap";
-import Search from "./blog/Search";
 import Router from "next/router";
-import nProgress from "nprogress";
-
+import NProgress from "nprogress";
+import { APP_NAME } from "../config";
+import { signout, isAuth } from "../actions/auth";
+import {
+	Collapse,
+	Navbar,
+	NavbarToggler,
+	NavbarBrand,
+	Nav,
+	NavItem,
+	NavLink,
+	UncontrolledDropdown,
+	DropdownToggle,
+	DropdownMenu,
+	DropdownItem,
+} from "reactstrap";
 import ".././node_modules/nprogress/nprogress.css";
+import Search from "./blog/Search";
 
-Router.onRouteChangeStart = (url) => nProgress.start();
-Router.onRouteChangeComplete = (url) => nProgress.done();
-Router.onRouteChangeError = (url) => nProgress.done();
+Router.onRouteChangeStart = (url) => NProgress.start();
+Router.onRouteChangeComplete = (url) => NProgress.done();
+Router.onRouteChangeError = (url) => NProgress.done();
 
 const Header = () => {
 	const [isOpen, setIsOpen] = useState(false);
@@ -27,14 +30,14 @@ const Header = () => {
 	const toggle = () => {
 		setIsOpen(!isOpen);
 	};
+
 	return (
 		<React.Fragment>
 			<Navbar color='light' light expand='md'>
 				<Link href='/'>
 					<NavLink className='font-weight-bold'>{APP_NAME}</NavLink>
 				</Link>
-
-				<NavbarToggler className='me-2' onClick={toggle} />
+				<NavbarToggler onClick={toggle} />
 				<Collapse isOpen={isOpen} navbar>
 					<Nav className='ml-auto' navbar>
 						<React.Fragment>
@@ -60,17 +63,17 @@ const Header = () => {
 							</React.Fragment>
 						)}
 
-						{isAuth() && isAuth().role === 1 && (
+						{isAuth() && isAuth().role === 0 && (
 							<NavItem>
-								<Link href='/admin'>
+								<Link href='/user'>
 									<NavLink>{`${isAuth().name}'s Dashboard`}</NavLink>
 								</Link>
 							</NavItem>
 						)}
 
-						{isAuth() && isAuth().role === 0 && (
+						{isAuth() && isAuth().role === 1 && (
 							<NavItem>
-								<Link href='/user'>
+								<Link href='/admin'>
 									<NavLink>{`${isAuth().name}'s Dashboard`}</NavLink>
 								</Link>
 							</NavItem>
@@ -80,12 +83,20 @@ const Header = () => {
 							<NavItem>
 								<NavLink
 									style={{ cursor: "pointer" }}
-									onClick={() => signout(() => Router.push("/signin"))}
+									onClick={() => signout(() => Router.replace(`/signin`))}
 								>
 									Signout
 								</NavLink>
 							</NavItem>
 						)}
+
+						<NavItem>
+							<Link href='/user/crud/create'>
+								<NavLink className='btn btn-primary text-light'>
+									Write a blog
+								</NavLink>
+							</Link>
+						</NavItem>
 					</Nav>
 				</Collapse>
 			</Navbar>
